02fd16d73d88c1650bd181da920403b3d165c22b
Hsin-Yi Chen (hychen) <ossug.hychen@gmail.com>
2011-02-09 12:43:21 +0800

    Add/Update more metadata
    * Indicate Authors
    * Make description more clear
    * Add classfiers
    * Copyright year from 2010 to 2011

--- setup.py
+++ setup.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python
 # -*- encoding=utf8 -*-
 #
-# Copyright © 2010 Hsin Yi Chen
+# Copyright © 2011 Hsin Yi Chen
 from distutils.core import setup
 
 setup(
     name = 'ucltip',
     version = open('VERSION.txt').read().strip(),
-    description = 'Use command line tool in Python',
+    description = 'A library to help making command line tool Python binding faster',
     long_description=open('README.rst').read(),
     author = 'Hsin-Yi Chen 陳信屹 (hychen)',
     author_email = 'ossug.hychen@gmail.com',
-    license = 'BSD License',
+    url='http://github.com/hychen/ucltip',
+    license = 'BSD-2-clause License',
     packages=['ucltip'],
+    classifiers = [
+      "Development Status :: 3 - Alpha",
+      "Intended Audience :: Developers",
+      "License :: OSI Approved :: BSD License",
+      "Operating System :: OS Independent",
+      "Programming Language :: Python",
+      "Programming Language :: Python :: 2.5",
+      "Programming Language :: Python :: 2.6",
+      "Topic :: Software Development :: Libraries :: Python Modules",
+    ]
 )
